c4db9fe9e2f1c6e88e8d0acc030de5b752a5a883
SpareLeg <mr.oo.leg@gmail.com>
2025-10-29 15:22:42 +0200

    init

---
+++ actix-form-serde/src/main.rs
@@ -0,0 +1,58 @@
+use actix_web::{App, HttpResponse, HttpServer, get, post, web};
+use serde::Deserialize;
+
+const PORT: u16 = 8080;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Serving on http://0.0.0.0:{}", PORT);
+    HttpServer::new(|| App::new().service(index).service(gcd_result))
+        .bind(("0.0.0.0", PORT))?
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
+            <title>GCD Calculator</title>
+            <form method="post">
+            <input type="text" name="n"/>
+            <input type="text" name="m"/>
+            <button type="submit">Compute GCD</button>
+            </form>
+        "#,
+    )
+}
+
+#[post("/")]
+async fn gcd_result(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+    HttpResponse::Ok().content_type("text/html").body(format!(
+        "The greatest common divisor of the numbers {} and {} is <b>{}</b>",
+        form.n,
+        form.m,
+        gcd(form.n, form.m)
+    ))
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    while m != 0 {
+        if m < n {
+            (m, n) = (n, m);
+        }
+        m %= n;
+    }
+    n
+}
---
+++ grep-lite/src/main.rs
@@ -0,0 +1,41 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+use clap::Parser;
+use regex::Regex;
+
+/// Lite version of grep
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The pattern to search for
+    #[arg()]
+    pattern: String,
+    /// File to search
+    #[arg()]
+    input: Option<String>,
+}
+
+fn main() {
+    let args = Args::parse();
+    let re = Regex::new(&args.pattern).unwrap();
+    if let Some(input) = args.input {
+        let f = File::open(input).unwrap();
+        process_lines(BufReader::new(f), re);
+    } else {
+        let stdin = io::stdin();
+        process_lines(stdin.lock(), re);
+    }
+}
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line in reader.lines() {
+        if let Ok(line) = line
+            && re.is_match(&line)
+        {
+            println!("{}", line)
+        }
+    }
+}
